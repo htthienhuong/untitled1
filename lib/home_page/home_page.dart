6e7a22ac0e20591c3d7f6d62fbd396070b29168e
lehuynhphat2808 <52100828@student.tdtu.edu.vn>
2024-05-14 00:02:32 +0700

    add home UI

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +15,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xffe2e9ff),
+        backgroundColor: const Color(0xffe2e9ff),
         title: const Text(
           'Home',
           style:
@@ -20,6 +23,113 @@ class _HomePageState extends State<HomePage> {
         ),
         centerTitle: true,
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(color: Colors.black)),
+              child: Row(
+                children: [
+                  const Expanded(child: TextField()),
+                  const VerticalDivider(
+                    color: Colors.black,
+                  ),
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+                ],
+              ),
+            ),
+            const Text(
+              'Community',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(top: 8),
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: const Color(0xffdce1ef),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Topic Name',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                  color: const Color(0xffacbdd0),
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: const Text('0 words'),
+                            ),
+                          ],
+                        ),
+                        const Text('Description'),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 50,
+                              width: 50,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50),
+                                child: FadeInImage(
+                                  placeholder:
+                                      AssetImage('assets/images/htth_avt.png'),
+                                  image: NetworkImage('xxx'),
+                                  imageErrorBuilder: (context, error,
+                                          stackTrace) =>
+                                      Image.asset('assets/images/htth_avt.png'),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  final List<String> myCoolStrings = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Some other item'
+  ];
+  final List<String> _results = []; // Empty at start
+
+  void _handleSearch(String input) {
+    _results.clear();
+    for (var str in myCoolStrings) {
+      if (str.toLowerCase().contains(input.toLowerCase())) {
+        setState(() {
+          _results.add(str);
+        });
+      }
+    }
   }
 }
